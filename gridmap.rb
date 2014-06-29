ed82a684ef3d43089dbeb859d7dd87b43e561cc2
Ben Gorman <8076bgorman@gmail.com>
2014-06-29 13:49:41 +0100

    v1

--- gridmap.rb
+++ gridmap.rb
@@ -1,7 +1,7 @@
-require 'gosu'
+require 'gosu' 
 
 class Gridmap < Array
-	attr_reader :tile_width, :tile_height, :width, :height
+	attr_reader :tile_width, :tile_height, :grid_width, :grid_height, :pixel_width, :pixel_height
   	def initialize(map_path, symbol_hash, tile_width, tile_height)
   		@tile_width, @tile_height = tile_width, tile_height
 
@@ -14,61 +14,34 @@ class Gridmap < Array
 	    end
 
 	    #Since all lines should contain the same number of characters, the width is set by counting the number of characters in the first line.
-		@width = self.first.count * tile_width
-		@height = self.count * tile_height
+		@grid_width  = self.first.count - 1
+		@grid_height = self.count - 1
+
+		@pixel_width = @grid_width + 1 * tile_width
+		@pixel_height = @grid_height + 1 * tile_height
 	end
 
-	def draw_tiles(window)
-		self.each {|line| line.each {|tile| tile.draw(nil, window)}}
+	def orientate_tiles(window, image_hash)
+		
+	end
+
+	def draw_tiles
+		self.each {|line| line.each {|tile| tile.draw_rot}}
 	end
 end
 
 class Tile
 	attr_reader :x, :y, :z
-	attr_accessor :details, :image, :image_path
+	attr_accessor :details, :connections, :orientation, :angle, :image
 	def initialize(x, y, width, height, details)
 		@x, @y, @width, @height, @details = x, y, width, height, details
-
-		if details.is_a?(Hash)
-			if details[:image]
-				@image = details[:image]
-			elsif details[:image_path]
-				@image_path = details[:image_path]
-			end
-		end
-	end
-
-	def draw(*p) #p should contain z, then image or window
-		unless p.empty?
-			process_optional_params(p)
-		end
 		@z ||= ZOrder::TILE
-		@image.draw @x * @width, @y * @height, @z
+		@angle ||= 0
 	end
 
-	def draw_rot(offset_x, offset_y, angle, *p) #p should contain z, then image or window
-		offset_x, offset_y, angle = p[0], p[1], p[2]
-		unless p.empty?
-			process_optional_params(p)
-		end
-		@z ||= ZOrder::TILE
-		@image.draw_rot @x * @width + offset_x, @y * @height + offset_y, @z, angle
-	end
-
-	def process_optional_params(p)
-		@z = p[0] if p[0] # if p isn't empty, z could still be set as nil.
-		
-		if p[1]
-			if p[1].class == Gosu::Image
-				@image = p[1]
-			elsif p[1].kind_of?(Gosu::Window)
-				#If a window is passed then we assume that the image path has already been set.
-				@image = Gosu::Image.new(p[1], @image_path, true)
-			else
-				raise StandardError, "Image parameter for tile draw method must be an image or a window: #{p[1].inspect}"
-			end
-		end
-
+	def draw_rot
+		# Half the height and half the width are added to the x, y co-ordinates because draw_rot draws the CENTRE of the image at the given co-ords.
+		@image.draw_rot @x * @width + @width / 2, @y * @height + @height / 2, @z, @angle
 	end
 end
 
@@ -80,33 +53,68 @@ end
 class Controller < Gosu::Window
 	def initialize
 		symbol_hash = {
-			'#' => {type: 'road', image_path: 'assets/images/road.bmp'}
+			'#' => {type: 'road', connected: true},
+			'G' => {type: 'garage', connected: false},
+			'T' => {type: 'tower', connected: false},
 		}
-    	symbol_hash.default = 'blank'
+		symbol_hash.default = {type: 'background', connected: false}
+		image_path = "assets/images/"
+		images = {
+			straight: 		'assets/images/straight_road.bmp',
+			corner: 		'assets/images/corner_junction.bmp',
+			tri: 			'assets/images/tri_junction.bmp',
+			quad: 			'assets/images/quad_junction.bmp',
+		}
+		images.default = 'assets/images/background.bmp'
 
 		@grid = Gridmap.new("assets/roadmap.txt", symbol_hash, 30, 30)
-		
-		super(@grid.width, @grid.height, fullscreen = false)
-
-		#@grid.each {|line| line.each {|tile| tile.orientate(@grid)}}
-
-		new_image = Proc.new {|name| Gosu::Image.new(self, "assets/images/#{name}.bmp", true)}
-    	@images = {
-			background: 	new_image.call('background'),
-			straight: 		new_image.call('straight_road'),
-			corner: 		new_image.call('corner_junction'),
-			tri: 			new_image.call('tri_junction'),
-			quad: 			new_image.call('quad_junction'),
-		}
+		#Window must be defined before images can be defined.
+		super(1000, 1000, fullscreen = false)
+		orientate_tiles(images)
 	end
 
+	def orientate_tiles(images)
+		#The check proc checks a tile with the given grid co-ordinates to see if it is connected or not.
+		#The logic here prevents a tile from being checked if it lies outside the grid.
+		check = Proc.new {|x, y| 	if y < 0 || x < 0 || y > @grid.grid_height || x > @grid.grid_width
+												false
+											else
+												@grid[y][x].details[:connected]
+											end }
+
+		@grid.each_with_index do |line, y|
+	      	line.each_with_index do |tile, x|
+	      		connections    = [false, false, false, false] # up right down left
+	      		connections[0] = check.call(x, y - 1) #up
+				connections[1] = check.call(x + 1, y) #right
+				connections[2] = check.call(x, y + 1) #down
+				connections[3] = check.call(x - 1, y) #left
+				tile.connections = connections
+
+				case connections.count(true)
+				when 1
+					orientation = :straight
+				when 2
+					orientation = :corner
+				when 3
+					orientation = :tri
+				when 4
+					orientation = :quad
+				else
+					orientation = :background
+				end
+
+				tile.image = Gosu::Image.new(self, images[orientation], true)
+				tile.orientation = orientation
+	     	end
+	    end
+	end
 
 	def update
 	end
 
 	def draw
-		@grid_image ||= record(@grid.width, @grid.height) { @grid.draw_tiles(self)}
-		@grid_image.draw 0, 0, 0
+		@grid.draw_tiles
 	end
 
 	def needs_cursor?
